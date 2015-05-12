8f4ba591c269096f88c2e798eded9ee81c194867
Guiann Borgonia <gborgonia.com>
2015-05-12 13:06:16 +0800

    5-11 updates

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,5 +2,5 @@ class Dish < ActiveRecord::Base
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	validates :name, :description, :price, :category, presence: true
-	validates_numericality_of :price
+	validates_numericality_of :price, allow_blank: true
 end
