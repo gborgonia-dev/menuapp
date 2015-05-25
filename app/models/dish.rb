6dd4b76e1a85b7f8dfbe43b1811c82dfe4b44c6d
Guiann Borgonia <gborgonia.com>
2015-05-25 16:06:47 +0800

    commit untracked files

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,7 @@
 class Dish < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing.jpg"
+	has_attached_file :avatar, :styles => { :medium => "300x300>" }, :default_url => "missing.jpg"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-	validates :name, :description, :price, :category, presence: true
+	validates :name, :description, :price, :category, :avatar, presence: true
 	validates_numericality_of :price, allow_blank: true
 end
