37fbf28325e4c8463e3c1e75f9ec3c21443b1017
Guiann Borgonia <gborgonia.com>
2015-06-02 17:55:57 +0800

    order feature

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,34 @@
 class OrderItemsController < ApplicationController
+
+	def create
+		@order = current_order
+		@order_item = @order.order_items.new(order_item_params)
+		@order_item.save
+		session[:order_id] = @order_id
+	end
+
+	def update
+		@order = current_order
+		@order_item = @order.order_items.find(params[:id])
+		@order_item.update_attributes(order_item_params)
+		@order_items = @order.order_items
+	end
+
+	def destroy
+		@order = current_order
+		@order_item = @order.order_items.find(params[:id])
+		@order_item.destroy
+		@order_items = @order.order_items
+	end
+
+	private
+
+	def set_order_item
+		@order_item = OrderItem.find(params[:id])
+	end
+
+	def order_item_params
+		params.require(:order_item).permit(:quantity, :dish_id)
+	end
+
 end
--- db/migrate/20150525074820_create_order_items.rb
+++ db/migrate/20150525074820_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-    	t.float :price
+    	t.float :quantity
     	t.belongs_to :dish
     	t.belongs_to :order
     end
